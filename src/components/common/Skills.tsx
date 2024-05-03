732160c9e531d4f0bad0e690c95695d0ce683bc7
HyunJinNo <nhj9831@naver.com>
2024-05-03 17:28:29 +0900

    skills 컴포넌트 작성

--- src/components/common/Skills.tsx
+++ src/components/common/Skills.tsx
@@ -11,57 +11,65 @@ const Skills = () => {
         <h1>My Skills</h1>
       </div>
 
-      <h2>Front-end</h2>
-      <div className="cardViewGroup">
-        {BADGE_IMAGES.frontend.map((value) => (
-          <Image
-            className="cardView"
-            key={value.alt}
-            src={value.src}
-            alt={value.src}
-            width={value.width}
-            height={100}
-          />
-        ))}
+      <div className="category" data-aos="flip-down">
+        <h2>Front-end</h2>
+        <div className="cardViewGroup">
+          {BADGE_IMAGES.frontend.map((value) => (
+            <Image
+              className="cardView"
+              key={value.alt}
+              src={value.src}
+              alt={value.src}
+              width={value.width}
+              height={100}
+            />
+          ))}
+        </div>
       </div>
-      <h2>Back-end</h2>
-      <div className="cardViewGroup">
-        {BADGE_IMAGES.backend.map((value) => (
-          <Image
-            className="cardView"
-            key={value.alt}
-            src={value.src}
-            alt={value.src}
-            width={value.width}
-            height={100}
-          />
-        ))}
+      <div className="category" data-aos="flip-down">
+        <h2>Back-end</h2>
+        <div className="cardViewGroup">
+          {BADGE_IMAGES.backend.map((value) => (
+            <Image
+              className="cardView"
+              key={value.alt}
+              src={value.src}
+              alt={value.src}
+              width={value.width}
+              height={100}
+            />
+          ))}
+        </div>
       </div>
-      <h2>Mobile</h2>
-      <div className="cardViewGroup">
-        {BADGE_IMAGES.mobile.map((value) => (
-          <Image
-            className="cardView"
-            key={value.alt}
-            src={value.src}
-            alt={value.src}
-            width={value.width}
-            height={100}
-          />
-        ))}
+      <div className="category" data-aos="flip-down">
+        <h2>Mobile</h2>
+        <div className="cardViewGroup">
+          {BADGE_IMAGES.mobile.map((value) => (
+            <Image
+              className="cardView"
+              key={value.alt}
+              src={value.src}
+              alt={value.src}
+              width={value.width}
+              height={100}
+            />
+          ))}
+        </div>
       </div>
-      <h2>Version Control</h2>
-      <div className="cardViewGroup">
-        {BADGE_IMAGES.versionControl.map((value) => (
-          <Image
-            className="cardView"
-            key={value.alt}
-            src={value.src}
-            alt={value.src}
-            width={value.width}
-            height={100}
-          />
-        ))}
+      <div className="category" data-aos="flip-down">
+        <h2>Version Control</h2>
+        <div className="cardViewGroup">
+          {BADGE_IMAGES.versionControl.map((value) => (
+            <Image
+              className="cardView"
+              key={value.alt}
+              src={value.src}
+              alt={value.src}
+              width={value.width}
+              height={100}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
